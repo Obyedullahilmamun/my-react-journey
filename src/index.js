6dadf5ed4b6e565a0261f49c493cbd11b3eacb07
Obyedullahilmamun <obyedullahilmamun@gmail.com>
2025-08-05 13:35:25 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const todoTitle = 'Call Family';
+const todoDescription = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a /galley of type and scrambled it to make a type specimen book.';
+// const date = '8/5/2025';
+const date = new Date();
+const dateName = date.getDate();
+const monthName = date.getMonth();
+const currentYear = date.getFullYear();
+
+// const headingStyle = {
+//     backgroundColor: "purple",
+//     color: "white",
+//     textAlign: "center",
+//     padding: "15px"
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
+    <div>
+    <h1 className="headingStyle largeText">Welcome</h1>
+    <h3>{todoTitle}</h3>
+    <h3>{todoDescription}</h3>
+    <h3 style={{color: 'green'}}>Hello</h3>
+    <p>{dateName + "/" + monthName + "/" + currentYear}</p>
 
-    <h3>Hello</h3>
-
+</div>
 );
+
