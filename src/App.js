0b4fa7e04ef479a167f9e9e68dbc893f0ad4d964
Obyedullahilmamun <obyedullahilmamun@gmail.com>
2025-08-07 18:21:32 +0600

    apply Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,53 +1,12 @@
-// import React from "react"
-// import Card from "./components/Card";
-// import Data from "./data.json"; 
-// function App() {
-
-//     return <div>
-//                 <h1 className="headingStyle largeText">Welcome</h1>
-//                  {Data.map((item, index) => <Card key={index} titleText={item.title} descText={item.desc} /> )}  
-//     </div>
- 
-// } 
-
-// export default App;
-
-import React from 'react'
+import React from 'react';
 import './index.css';
+import { ReactBootstrap } from './components/React_Bootstrap/ReactBootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-import { FaFacebook, FaYoutube  } from "react-icons/fa";
-
-
-import Card1 from './components/Card1'
-import Card2 from './components/Card2'
-
-function App(){
+export default function App() {
     return (
         <div>
-            <Card1 name="Card1" desc="this is card1" />
-            <Card2 name="Card2" />
-
-        <br /> 
-        <span className="icon">
-
-        < FaFacebook/>
-
-        </span>
-
-
-         <span className="icon">
-
-        < FaYoutube/>
-
-        </span>
-
-
-
+             <ReactBootstrap />
         </div>
-
-    )
-
-} 
-
-export default App;
+            )
+}
---
+++ src/components/React_Bootstrap/ReactBootstrap.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
+
+export const ReactBootstrap = () => {
+  return (
+    <div>
+      {/* Cards Row */}
+      <div style={{ display: 'flex', gap: '1rem', marginBottom: '2rem' }}>
+        <Card style={{ width: "20rem" }}>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>ReactBootstrap</Card.Text>
+            <p>lorem text lorem text test</p>
+            <Button>Learn More</Button>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "20rem" }}>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>ReactBootstrap</Card.Text>
+            <p>lorem text lorem text test</p>
+            <Button>Learn More</Button>
+          </Card.Body>
+        </Card>
+      </div>
+
+      {/* Table Below Cards */}
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>This is</td>
+            <td>Name</td>
+            <td>@name</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>This is</td>
+            <td>Name</td>
+            <td>@name</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>This is</td>
+            <td>Name</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+};
